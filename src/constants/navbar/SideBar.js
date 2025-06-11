70f6b2599f66706d5c1da856023a298855fe8908
Eity2021 <Eity2021akter@gmail.com>
2025-06-11 14:19:00 +0600

    integrate logo

--- src/constants/navbar/SideBar.js
+++ src/constants/navbar/SideBar.js
@@ -11,6 +11,8 @@ import {
 import { Link } from "react-router-dom";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import Logout from "../../image/icons/Logout.png";
+import headerLogo from "../../image/logo/headerLogo.png";
+
 export default function SideBar({ routes }) {
   const [openMenus, setOpenMenus] = useState({});
 
@@ -31,25 +33,30 @@ export default function SideBar({ routes }) {
           height: "100%",
         }}
       >
-        <p>Logo</p>
-        {/* Close Button */}
+
 
         {/* Navigation Items */}
         <Box sx={{ width: 250, height: "100%", p: 2 }}>
-          <Typography variant="h6" sx={{ mb: 2 }}>
-            Dashboard
-          </Typography>
+      <Box>
+        <img src={headerLogo} alt="logo" />
+      </Box>
 
-          <List component="">
+          <List component="nav" sx={{fontSize:'18px',fontWeight:'400px'}}>
             {routes.map((link, index) => (
-              <Box key={index}>
-                <ListItemButton sx={{fontSize:'18px',fontWeight:'400px'}}
+              <Box key={index} >
+                <ListItemButton 
+                       sx={{
+  backgroundColor: 'transparent',
+  '&:hover': {
+    backgroundColor: '#EDF3F7',
+  },
+}}
                   onClick={() => (link.children ? toggleMenu(link.name) : null)}
                   component={!link.children ? Link : "div"}
                   to={!link.children ? link.path : undefined}
                 >
                   {link.icon && (
-                    <ListItemIcon sx={{ color: "#cfcfcf", minWidth: 32 }}>
+                    <ListItemIcon sx={{ color: "#12a9b2",minWidth: 32 }}>
                       {link.icon}
                     </ListItemIcon>
                   )}
@@ -73,13 +80,16 @@ export default function SideBar({ routes }) {
                           sx={{
                             pl: 4,
                             color: "#333",
-                            fontSize:'14px',fontWeight:'400px'
-                            // '&:hover': { backgroundColor: '#444' },
+                            fontSize:'12px',fontWeight:'400px',
+                            backgroundColor: 'transparent',
+                           '&:hover': {
+                           backgroundColor: '#EDF3F7',
+                           }
                           }}
                         >
                           {child.icon && (
                             <ListItemIcon
-                              sx={{ color: "#cfcfcf", minWidth: 32 }}
+                              sx={{ color: "#0095FF", minWidth: 32 }}
                             >
                               {child.icon}
                             </ListItemIcon>
@@ -97,7 +107,7 @@ export default function SideBar({ routes }) {
                 <img
                   src={Logout}
                   alt="dashboard icon"
-                  style={{paddingRight: "10px" }}
+                  style={{width:18, height:18, paddingRight: "10px" }}
                 />{" "}
                 Logout
               </ListItemButton>
