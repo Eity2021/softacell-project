52241741ec5874edd2d0b20848d46e5c9d31f3e3
Eity2021 <Eity2021akter@gmail.com>
2025-06-12 12:01:23 +0600

    working form page

--- src/pages/eventList/EventList.js
+++ src/pages/eventList/EventList.js
@@ -1,13 +1,13 @@
 
-import { Box } from '@mui/material';
-import EventBanner from './EventBanner';
-import EventSearch from './EventSearch';
+// import { Box } from '@mui/material';
+// import EventBanner from './EventBanner';
+// import EventSearch from './EventSearch';
 
-export default function EventList() {
-  return (
-<Box sx={{ minHeight: "100vh", pt: 2 }}>
-   <EventBanner></EventBanner>
-   <EventSearch></EventSearch>
-</Box>
-  )
-}
+// export default function EventList() {
+//   return (
+// <Box sx={{ minHeight: "100vh", pt: 2 }}>
+//    <EventBanner></EventBanner>
+//    <EventSearch></EventSearch>
+// </Box>
+//   )
+// }
--- src/pages/eventList/EventSearch.js
+++ src/pages/eventList/EventSearch.js
@@ -1,92 +1,92 @@
-import React from "react";
-import {
-  Box,
-  Button,
-  IconButton,
-  MenuItem,
-  Select,
-  Typography,
-  Paper,
+// import React from "react";
+// import {
+//   Box,
+//   Button,
+//   IconButton,
+//   MenuItem,
+//   Select,
+//   Typography,
+//   Paper,
 
-} from "@mui/material";
+// } from "@mui/material";
 
-import {
-  ChevronLeft,
-  ChevronRight,
-} from "lucide-react";
-import EventButton from "./EventButton";
-import EventAllListed from "./EventAllListed";
+// import {
+//   ChevronLeft,
+//   ChevronRight,
+// } from "lucide-react";
+// import EventButton from "./EventButton";
+// import EventAllListed from "./EventAllListed";
 
 
-export default function EventSearch() {
+// export default function EventSearch() {
 
-  return (
-    <Box>
-    <Box sx={{display:'flex',mt:2}}>
-        <Typography sx={{fontWeight:500,fontSize:'20px', fontFamily:'poppins',color:'#0195FF',pr:.5}}> 100,000+   </Typography> 
-      <Typography sx={{fontWeight:500,fontSize:'20px', fontFamily:'poppins',color:'#003255' ,mb:2}} >
-          events happening this month
-        </Typography>
-     </Box>
-<EventButton></EventButton>
- <Box sx={{display:'flex', gap:1, justifyContent:'flex-end' ,mt:3}}>
-   <Button
-  variant="outlined" 
-  sx={{
-    width:'130px',
-   fontSize:'16px',
-   fontWeight:'poppins',
-    borderRadius: "33px",
-    borderColor: "#0195FF", 
-    color: "#0195FF", 
-    "&:hover": {
-      borderColor: "#2196f3", 
-    },
-    "&:focus": {
-      borderColor: "#2196f3", 
-      borderWidth: "1px", 
+//   return (
+//     <Box>
+//     <Box sx={{display:'flex',mt:2}}>
+//         <Typography sx={{fontWeight:500,fontSize:'20px', fontFamily:'poppins',color:'#0195FF',pr:.5}}> 100,000+   </Typography> 
+//       <Typography sx={{fontWeight:500,fontSize:'20px', fontFamily:'poppins',color:'#003255' ,mb:2}} >
+//           events happening this month
+//         </Typography>
+//      </Box>
+// <EventButton></EventButton>
+//  <Box sx={{display:'flex', gap:1, justifyContent:'flex-end' ,mt:3}}>
+//    <Button
+//   variant="outlined" 
+//   sx={{
+//     width:'130px',
+//    fontSize:'16px',
+//    fontWeight:'poppins',
+//     borderRadius: "33px",
+//     borderColor: "#0195FF", 
+//     color: "#0195FF", 
+//     "&:hover": {
+//       borderColor: "#2196f3", 
+//     },
+//     "&:focus": {
+//       borderColor: "#2196f3", 
+//       borderWidth: "1px", 
      
-    },
-  }}
->
-  Filter
-</Button>
-     <Button        
-     variant="outlined"
-            fullWidth
-            sx={{
-              width: "218px",
-              height: "60px",
-              borderRadius: "12px",
-              backgroundColor: "#F4F8EE",
-              fontWeight: 400,
-              fontSize: "18px",
-              fontFamily: "poppins",
-            }}>My Events</Button>
-    </Box>
+//     },
+//   }}
+// >
+//   Filter
+// </Button>
+//      <Button        
+//      variant="outlined"
+//             fullWidth
+//             sx={{
+//               width: "218px",
+//               height: "60px",
+//               borderRadius: "12px",
+//               backgroundColor: "#F4F8EE",
+//               fontWeight: 400,
+//               fontSize: "18px",
+//               fontFamily: "poppins",
+//             }}>My Events</Button>
+//     </Box>
 
 
-     <EventAllListed></EventAllListed>
+//      <EventAllListed></EventAllListed>
 
-        {/* Pagination */}
-        <Box display="flex" justifyContent="space-between" alignItems="center" mt={2}>
-          <Box display="flex" alignItems="center" gap={1}>
-            <Typography variant="body2">Rows per page:</Typography>
-            <Select size="small" defaultValue={10}>
-              <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={10}>10</MenuItem>
-              <MenuItem value={20}>20</MenuItem>
-            </Select>
-          </Box>
-          <Box display="flex" alignItems="center" gap={1}>
-            <Typography variant="body2">1-2 of 2</Typography>
-            <IconButton disabled><ChevronLeft fontSize="small" /></IconButton>
-            <IconButton disabled><ChevronRight fontSize="small" /></IconButton>
-          </Box>
-        </Box>
-    </Box>
-  )
-}
+//         {/* Pagination */}
+//         <Box display="flex" justifyContent="space-between" alignItems="center" mt={2}>
+//           <Box display="flex" alignItems="center" gap={1}>
+//             <Typography variant="body2">Rows per page:</Typography>
+//             <Select size="small" defaultValue={10}>
+//               <MenuItem value={5}>5</MenuItem>
+//               <MenuItem value={10}>10</MenuItem>
+//               <MenuItem value={20}>20</MenuItem>
+//             </Select>
+//           </Box>
+//           <Box display="flex" alignItems="center" gap={1}>
+//             <Typography variant="body2">1-2 of 2</Typography>
+//             <IconButton disabled><ChevronLeft fontSize="small" /></IconButton>
+//             <IconButton disabled><ChevronRight fontSize="small" /></IconButton>
+//           </Box>
+//         </Box>
+//     </Box>
+//   )
+// }
 
 
 
